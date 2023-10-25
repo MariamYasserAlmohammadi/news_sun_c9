09d3f66739895613429897986e111d616cc504c5
DESKTOP-Q2BPRDF\NEXT <mariamyasser3092@gmail.com>
2023-10-25 17:57:39 +0300

    + finish Drawer

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:news_sun_c9/data/modal/SourcesResponse.dart';
 import 'package:news_sun_c9/data/modal/category_dm.dart';
 import 'package:news_sun_c9/ui/screens/home/tabs/categories/categories_tab.dart';
 import 'package:news_sun_c9/ui/screens/home/tabs/news/news_tab.dart';
+import 'package:news_sun_c9/ui/screens/home/tabs/settings/settings_tab.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = "home_screen" ;
@@ -26,31 +27,33 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
 
-    return  WillPopScope(
-      onWillPop:(){
-        if(currentTab is! CategoriesTab){
-          currentTab ==CategoriesTab(onCategoryClick);
-          setState(() {
+    return  SafeArea(
+      child: WillPopScope(
+        onWillPop:(){
+          if(currentTab is! CategoriesTab){
+            currentTab ==CategoriesTab(onCategoryClick);
+            setState(() {
 
-          });
-          return Future.value(false);
-        }else{
-          return Future.value(true);
-        }
+            });
+            return Future.value(false);
+          }else{
+            return Future.value(true);
+          }
 
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("News App"),
-          elevation: 0,
-          actions: [],
-          centerTitle: true,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(bottomRight:Radius.circular(22),bottomLeft: Radius.circular(22) )
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("News App"),
+            elevation: 0,
+            actions: [],
+            centerTitle: true,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(bottomRight:Radius.circular(22),bottomLeft: Radius.circular(22) )
+            ),
           ),
+          body:currentTab ,
+          drawer: buildDrawer(),
         ),
-        body:currentTab ,
-        drawer: buildDrawer()
       ),
     );
   }
@@ -62,8 +65,54 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
   Widget buildDrawer(){
-    return Container(
+    return  Drawer(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+              child: Center(
+                child: Text("News app" ,style:TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ) ,),
+              )),
+          buildDrawerRow(Icons.list, "Categories", (){
+            currentTab =CategoriesTab(onCategoryClick);
+            setState(() {
 
+            });
+            Navigator.pop(context);
+          }),
+          const SizedBox(height: 24,),
+          buildDrawerRow(Icons.list, "Categories", (){
+            currentTab =const SettingsTab();
+            setState(() {
+
+            });
+            Navigator.pop(context);
+          }),
+        ],
+      ),
     );
+  }
+   Widget buildDrawerRow(IconData iconData ,String title ,Function onClick){
+   return InkWell(
+     onTap: (){
+       onClick();
+     },
+     child: Row(
+        children: [
+          const SizedBox(width: 16,),
+          Icon(iconData,color: Colors.black ,size: 35,),
+          const SizedBox(width: 16,),
+          Text(title , style: const TextStyle(
+            color: Colors.black ,fontWeight: FontWeight.bold
+          ),)
+        ],
+      ),
+   );
   }
 }
